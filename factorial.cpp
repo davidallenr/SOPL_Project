d7068426b20d5022f28cac55f7c92ea0362faf54
David Russell <davidallenr@gmail.com>
2022-04-27 10:21:16 -0400

    initial commit Hello World

---
+++ factorial.cpp
@@ -0,0 +1,14 @@
+/**
+ * @file factorial.cpp
+ * @author David Russell
+ * @brief Factorial in CPP
+ * @version 0.1
+ * @date 2022-04-27
+ *
+ */
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+}
