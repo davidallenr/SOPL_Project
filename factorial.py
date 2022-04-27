8b89a1122c1f2696588f6aadad67f475909dda72
David Russell <davidallenr@gmail.com>
2022-04-27 15:51:48 -0400

    More comments

--- factorial.py
+++ factorial.py
@@ -1,3 +1,17 @@
+"""
+@file factorial.py
+@author David Russell
+@brief Factorial in Python
+@version 0.1
+@date 2022-04-27
+"""
+
+# This is an iterative approach to factorial
+# Compared to the recursive approach in the C++
+# It relies on the for loop and stack management to
+# compute the factorial of n
+
+
 def factorial(n):
     result = 1
     for i in range(1, n+1):
